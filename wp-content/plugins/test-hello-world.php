529f3bb09811b1b3c26938ea72145c5ee82f9512
flyingbest <taeyoon4715@gmail.com>
2017-02-19 13:32:30 +0000

    plugins test

---
+++ wp-content/plugins/test-hello-world.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: taeyoon-hello-world
+Description: This is taeyoon's plugin
+*/
+
+add_action(
+	'admin_notice',
+	function() {
+		echo '<div class="notice notice-success"><p>Hello, taeyoon!</p></div>';
+	}
+);
+
+
